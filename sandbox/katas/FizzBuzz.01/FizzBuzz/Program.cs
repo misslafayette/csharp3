75f84c3500d286985bb26f5f155e2b85a08d0ffd
Anita <166741974+misslafayette@users.noreply.github.com>
2024-09-29 16:16:50 +0200

    homework 1

--- sandbox/katas/FizzBuzz.01/FizzBuzz/Program.cs
+++ sandbox/katas/FizzBuzz.01/FizzBuzz/Program.cs
@@ -1,23 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-public class FizzBuzz
-{
-    public void CountTo(int lastNumber)
-    {
-        for (int actualNumber = 1, actualNumber < lastNumber, actualNumber++)
-        {
-            if ( lastNumber % 3 == 0 && lastNumber % 5 == 0) 
-            {
-            Console.WriteLine("FizzBuzz");
-            }
-            else if ( actualNumber % 3 == 0) 
-            {
-            Console.WriteLine("Fizz");
-            }
-            else if (actualNumber % 5 == 0)
-            {
-            Console.WriteLine("Buzz");
-            }
-            Console.WriteLine(actualNumber);
-       }
-    }
-}
+
+
+FizzBuzz fizzBuzz = new FizzBuzz();
+fizzBuzz.CountTo(15);
