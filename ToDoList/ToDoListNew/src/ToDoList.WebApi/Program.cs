2dd4ff744af06c35244e240776f7bd1139baff14
Anita <166741974+misslafayette@users.noreply.github.com>
2024-10-21 17:38:28 +0200

    lesson 3

--- ToDoList/ToDoListNew/src/ToDoList.WebApi/Program.cs
+++ ToDoList/ToDoListNew/src/ToDoList.WebApi/Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/nazdarSvete", () => "Nazdar svete x2!");
 
 app.Run();
